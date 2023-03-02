e6375bcc4c2f4f7753e94b2bf3ed93628caf2727
tony.li <tony@choco-up.com>
2023-03-03 00:04:11 +0800

    test(testing): add more test cases

--- dictionary_test.go
+++ dictionary_test.go
@@ -2,6 +2,7 @@ package crdt
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,10 +16,23 @@ func TestAdd(t *testing.T) {
 
 	// Add multiple values
 	d.Add("foo", "bar")
+	d.Add("hello", "new world")
 	element, _ = d.Lookup("hello")
 	element2, _ := d.Lookup("foo")
-	assert.Equal(t, "world", (*element).GetValue(), "Should be the same value")
+	assert.Equal(t, "new world", (*element).GetValue(), "Should be the updated value")
 	assert.Equal(t, "bar", (*element2).GetValue(), "Should be the same value")
+
+	// custom adding an older elment
+	dt := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
+	d.AddAt("foo", "old bar", dt)
+	element, _ = d.Lookup("foo")
+	assert.Equal(t, "bar", (*element).GetValue(), "Should be the newer value")
+
+	// custom adding a newer elment
+	d.AddAt("foo", "new bar", time.Now().Add(1000))
+	d.AddAt("foo", "old bar", time.Now())
+	element, _ = d.Lookup("foo")
+	assert.Equal(t, "new bar", (*element).GetValue(), "Should be the newer value")
 }
 
 func TestAddAndRemove(t *testing.T) {
@@ -38,9 +52,33 @@ func TestAddAndRemove(t *testing.T) {
 func TestMerge(t *testing.T) {
 	d := NewDictionary()
 	d2 := NewDictionary()
+
+	// test new value exist on dictionary 2
 	d.Add("hello", "world")
 	d2.Add("hello", "new world")
+
+	// test new value exist on dictionary 1
+	d2.Add("foo", "bar")
+	d.Add("foo", "new bar")
+
+	// test value only exist on dictionary 1
+	d.Add("clean", "code")
+
+	// test value only exist on dictionary 2
+	d2.Add("cleaner", "architecture")
+
 	d3, _ := d.Merge(*d2)
+
 	element, _ := d3.Lookup("hello")
-	assert.Equal(t, "new world", (*element).GetValue(), "Should be the new value")
+	assert.Equal(t, "new world", (*element).GetValue(), "Should be the new value from dictionary 2")
+
+	element2, _ := d3.Lookup("foo")
+	assert.Equal(t, "new bar", (*element2).GetValue(), "Should be the new value from dictionary 1")
+
+	element3, _ := d3.Lookup("clean")
+	assert.Equal(t, "code", (*element3).GetValue(), "Should be the only value from dictionary 1")
+
+	element4, _ := d3.Lookup("cleaner")
+	assert.Equal(t, "architecture", (*element4).GetValue(), "Should be the only value from dictionary 2")
+
 }
